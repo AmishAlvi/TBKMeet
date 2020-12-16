837836a2ebe1a19a346c3811f231b997854c5670
AmishAlvi <amish.alvi@gmail.com>
2020-12-16 15:54:17 +0300

    rope

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> hello
+          Edit <code>src/App.js</code> hello boy
+          
         </p>
         <a
           className="App-link"
