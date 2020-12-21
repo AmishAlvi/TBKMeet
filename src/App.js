fce67f0fdbf18536c754dbe960836fd450156467
AmishAlvi <amish.alvi@gmail.com>
2020-12-21 11:31:04 +0300

    video handshake completed

--- src/App.js
+++ src/App.js
@@ -52,16 +52,67 @@ function App() {
     })
 
     socket.current.on("hey", (data) => {
-     
+      setReceivingCall(true);
+      setCaller(data.from);
+      setCallerSignal(data.signal);
     })
   }, []);
 
   function callPeer(id) {
-    
+    const peer = new Peer({
+      initiator: true,
+      trickle: false,
+      config: {
+
+        iceServers: [
+            {
+                urls: "stun:numb.viagenie.ca",
+                username: "sultan1640@gmail.com",
+                credential: "98376683"
+            },
+            {
+                urls: "turn:numb.viagenie.ca",
+                username: "sultan1640@gmail.com",
+                credential: "98376683"
+            }
+        ]
+    },
+      stream: stream,
+    });
+
+    peer.on("signal", data => {
+      socket.current.emit("callUser", { userToCall: id, signalData: data, from: yourID })
+    })
+
+    peer.on("stream", stream => {
+      if (partnerVideo.current) {
+        partnerVideo.current.srcObject = stream;
+      }
+    });
+
+    socket.current.on("callAccepted", signal => {
+      setCallAccepted(true);
+      peer.signal(signal);
+    })
+
   }
 
   function acceptCall() {
-    
+    setCallAccepted(true);
+    const peer = new Peer({
+      initiator: false,
+      trickle: false,
+      stream: stream,
+    });
+    peer.on("signal", data => {
+      socket.current.emit("acceptCall", { signal: data, to: caller })
+    })
+
+    peer.on("stream", stream => {
+      partnerVideo.current.srcObject = stream;
+    });
+
+    peer.signal(callerSignal);
   }
 
   let UserVideo;
