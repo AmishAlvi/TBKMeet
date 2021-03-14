45721c50deaef9b70c3fcf79eac428002e6e3f62
Noman Aslam <nomanaslamleghari@gmail.com>
2021-03-14 23:26:48 +0500

    invite users

--- server/controllers/meeting/meeting.js
+++ server/controllers/meeting/meeting.js
@@ -1,36 +1,37 @@
-const {Meeting} = require("../../models");
+const {Meeting,User} = require("../../models");
+const nodemailer = require("nodemailer");
+const email1 = require("../../config/prod").email;
+const password1 = require("../../config/prod").password;
 
 
 // TODO: add swagger
 module.exports = async (req, res, _next) => {
   const {
     title,
-    time,
     description,
     duration,
     location,
     date,
     topic,
     members,
-    notes,
-    owner
+    notes
 
   } = req.fields;
 
 
-  const meeting = new Meeting({title,time,description,duration,location,date,topic,members,notes,owner});
+  const meeting = new Meeting({title,description,duration,location,date,topic,members,notes});
   try {
-    meeting.owner = req.session.user;
     await meeting.save(async  (err) => {
       if (err) { return res.status(500).send({ msg: err.message }); }
       else{
         for(var i = 0;i<members.length;i++){
           await User.findOne({_id:members[i]}).then((user) =>{
+            if(!user)return res.status(409).json({message:"no user found"});
         var transporter = nodemailer.createTransport({ host:"webmail.remotify.co",port:465, secure:true, //Force TLS
       tls: {
           rejectUnauthorized: false
       }, auth: { user: email1, pass: password1 } });//add emails, and passwords from keys or env
-      var mailOptions = { from: email1, to: user.email, subject: 'Account Verification', text: 'Hello,\n\n' + 'Please verify your remotify account by clicking the link: \nhttp:\/\/' + req.headers.host + '\/auth\/confirmation\/' + token.token + '\n' };
+      var mailOptions = { from: email1, to: user.email, subject: 'Meeting Invitation', text: 'this is a good sign' };
       transporter.sendMail(mailOptions, function (err) {
           if (err) { return res.status(500).send({ msg: err.message }); }
       });
