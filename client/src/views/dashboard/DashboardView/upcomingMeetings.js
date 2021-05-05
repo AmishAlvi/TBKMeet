bf2d5e375de5c6b193ca516850ad974a2e055d84
AmishAlvi <amish.alvi@gmail.com>
2021-05-05 05:02:56 +0300

    changes

--- client/src/views/dashboard/DashboardView/upcomingMeetings.js
+++ client/src/views/dashboard/DashboardView/upcomingMeetings.js
@@ -62,7 +62,7 @@ const UpcomingMeetings = ({ className, ...rest }) => {
   const [meeting,setMeeting]=useState([]);
   useEffect(async () => {
     const result = await axios(
-        "http://localhost:81/meeting/getMeetings",
+        "http://localhost:81/meeting/getMeetings"
     );
     setMeeting(result.data.data)
   },[]);
