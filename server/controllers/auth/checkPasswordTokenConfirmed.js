98f15814e8edfa6f0a850da3ab9ca9c4f1dd35a5
nomanAslamLe <nomanaslamleghari@gmail.com>
2021-05-06 17:27:21 +0300

    bug fixed

--- server/controllers/auth/checkPasswordTokenConfirmed.js
+++ server/controllers/auth/checkPasswordTokenConfirmed.js
@@ -3,7 +3,7 @@ module.exports = async (req,res) =>{
     try{
     if(!req.session.userpass)return res.status(403).json({status:"forbidden"});
     else {
-        if(req.session.user.email == req.fields.email){
+        if(req.session.userpass.email == req.fields.email){
             return res.status(201).json({status:"success",message:"tokenclicked"});
         }
         else{
