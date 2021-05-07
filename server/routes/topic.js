877cf6f5e4c3262f2a432fb889252d05acfde8ff
nomanAslamLe <nomanaslamleghari@gmail.com>
2021-05-07 14:05:27 +0300

    get files, dlt topic and meeting func added

--- server/routes/topic.js
+++ server/routes/topic.js
@@ -1,6 +1,6 @@
 const {Router} = require("express");
 const http = require("http");
-const {topic,getTopic,getOneTopic,modifyTopic} = require("../controllers/topic");
+const {topic,getTopic,getOneTopic,modifyTopic,deleteTopic} = require("../controllers/topic");
 const {requireAuthentication,validateJson} = require("../middleware");
 
 const router = Router();
@@ -9,6 +9,7 @@ router.post("/topicSave", [topic]);
 router.get("/getTopic", [getTopic]);
 router.get("/getTopic/:id",[getOneTopic]);
 router.post("/modifyTopic/:id",[modifyTopic]);
+router.get("/deleteTopic/:id",[deleteTopic]);
 
 
 module.exports = router;
