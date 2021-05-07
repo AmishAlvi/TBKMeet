3c41125bbe10e840320a28dd57ea5a742539859b
AmishAlvi <amish.alvi@gmail.com>
2021-05-07 08:04:23 +0300

    chatroom optimization

--- index.js
+++ index.js
@@ -4,11 +4,36 @@ const http = require('http').createServer(app)
 const io = require('socket.io')(http)
 const PORT = process.env.PORT || 4000
 
+let users = []
+
 io.on('connection', socket => {
-  socket.on('message', ({ name, message }) => {
-    io.emit('message', { name, message })
+  socket.on('join room', (userID) => {
+    const user = {
+      userID, 
+      id: socket.id,
+    };
+    users.push(user);
+    io.emit("new user", users)
+  });
+
+  socket.on('join room', (roomID, cb) => {
+    socket.join(roomID);
+    cb(messages[roomID]);
+    socket.emit('joined', messages[roomID]);
   })
-  socket.emit('chat-message', 'hello world')
+
+  socket.on('send message', ({content, to, sender}) => {
+    const payload = {
+      content,
+      sender,
+    };
+    socket.to(to).emit('new message', payload)
+  })
+
+  socket.on('disconnect', () => {
+      users = users.filter(u => u.id !== socket.id);
+      io.emit('new user', users)
+  })
 })
 
 //server.listen(process.env.PORT || 8000, () => console.log('server is running on port 8000'));
