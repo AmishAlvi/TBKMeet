c51850fea46e53fff6ca76e2aec1e3980cbeea5d
AmishAlvi <amish.alvi@gmail.com>
2021-05-08 03:17:26 +0300

    room changed

--- client/src/views/attendMeeting/Room/timer.js
+++ client/src/views/attendMeeting/Room/timer.js
@@ -2,10 +2,9 @@ import React from 'react'
 import { useState, useEffect } from 'react';
 
 const Timer = (props) => {
-    const {initialHours = 0 , initialMinute = 0, initialSeconds = 0} = props;
-    const [minutes, setMinutes ] = useState(initialMinute);
+    const {initialMinute = 0,initialSeconds = 0} = props;
+    const [ minutes, setMinutes ] = useState(initialMinute);
     const [seconds, setSeconds ] =  useState(initialSeconds);
-    const [hours , setHours] = useState(initialHours);
     useEffect(()=>{
     let myInterval = setInterval(() => {
             if (seconds > 0) {
@@ -13,14 +12,7 @@ const Timer = (props) => {
             }
             if (seconds === 0) {
                 if (minutes === 0) {
-                    if(hours === 0 ) {
-                        clearInterval(myInterval)
-                    }
-                    else {
-                        setHours(hours - 1)
-                        setMinutes(59)
-                        setSeconds(59)
-                    }
+                    clearInterval(myInterval)
                 } else {
                     setMinutes(minutes - 1);
                     setSeconds(59);
@@ -35,8 +27,8 @@ const Timer = (props) => {
     return (
         <div>
         { minutes === 0 && seconds === 0
-            ? <h1>meeting ended</h1>
-            : <h1> {hours}:{minutes}:{seconds < 10 ?  `0${seconds}` : seconds}</h1> 
+            ? null
+            : <h1> {minutes}:{seconds < 10 ?  `0${seconds}` : seconds}</h1> 
         }
         </div>
     )
